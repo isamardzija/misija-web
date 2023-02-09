437d57a5720c66ec7f729e13e4af6c4b3d0bf756
Ivan <ivansam382@gmail.com>
2023-02-09 12:53:19 +0100

    add success/err msg

--- contact.js
+++ contact.js
@@ -1,19 +1,38 @@
 // KONTAKT
-const kontaktForma = document.querySelector("#kontakt");
 
-kontaktForma.addEventListener("submit", procKontakt);
+function contact() {
+  const kontaktForma = document.querySelector("#kontakt");
 
-async function procKontakt(e) {
-  e.preventDefault();
+  kontaktForma.addEventListener("submit", procKontakt);
+  async function procKontakt(e) {
+    e.preventDefault();
 
-  const data = new FormData(kontaktForma);
-
-  const res = await fetch(`https://cms.udrugamisija.hr/api/contact-messages`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(Object.fromEntries(data)),
-  }).then((res) => res.json());
-  kontaktForma.reset();
+    const data = new FormData(kontaktForma);
+    try {
+      const res = await fetch(
+        `https://cms.udrugamisija.hr/api/contact-messages`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(Object.fromEntries(data)),
+        }
+      ).then((res) => res.json());
+      kontaktForma.reset();
+      const successMessage = document.createElement("p");
+      successMessage.classList.add("success");
+      successMessage.textContent =
+        "Hvala na poruci! Potruditi ćemo se odgovoriti u najbržem mogućem roku.";
+      kontaktForma.appendChild(successMessage);
+    } catch (error) {
+      console.log(`Došlo je do pogreške! `, error);
+      const errorMessage = document.createElement("p");
+      errorMessage.classList.add("error");
+      errorMessage.textContent = "Slanje nije uspjelo!";
+      kontaktForma.appendChild(errorMessage);
+    }
+  }
 }
+
+contact();
--- join.js
+++ join.js
@@ -1,23 +1,36 @@
 // UCLANI SE
+function join() {
+  const uclaniSeForma = document.querySelector("#uclaniSe");
 
-const uclaniSeForma = document.querySelector("#uclaniSe");
-
-uclaniSeForma.addEventListener("submit", procUclanjenje);
-
-async function procUclanjenje(e) {
-  e.preventDefault();
-  const noviClanData = new FormData(uclaniSeForma);
-  console.log(noviClanData);
-
-  const res = await fetch(
-    "https://cms.udrugamisija.hr/api/membership-requests",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(Object.fromEntries(noviClanData)),
+  uclaniSeForma.addEventListener("submit", procUclanjenje);
+  async function procUclanjenje(e) {
+    e.preventDefault();
+    const noviClanData = new FormData(uclaniSeForma);
+    try {
+      const res = await fetch(
+        "https://cms.udrugamisija.hr/api/membership-requests",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(Object.fromEntries(noviClanData)),
+        }
+      ).then((res) => res.json());
+      uclaniSeForma.reset();
+      const successMessage = document.createElement("p");
+      successMessage.classList.add("success");
+      successMessage.textContent =
+        "Hvala na interesu! Javiti ćemo ti se ubrzo.";
+      uclaniSeForma.appendChild(successMessage);
+    } catch (error) {
+      console.log("Došlo je do pogreške!", error);
+      const errorMessage = document.createElement("p");
+      errorMessage.classList.add("error");
+      errorMessage.textContent = "Došlo je do pogreške!";
+      uclaniSeForma.appendChild(errorMessage);
     }
-  ).then((res) => res.json());
-  uclaniSeForma.reset();
+  }
 }
+
+join();
